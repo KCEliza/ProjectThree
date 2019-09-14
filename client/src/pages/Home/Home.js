bbe5a678fe481fd0fa5141868b43281438dc082b
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-09-14 09:49:44 -0600

    no more chuck norris

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,31 +1,17 @@
 import React, { Component } from "react";
-import { Button } from "reactstrap";
 import API from "../../utils/API";
-import Joke from "../../components/Joke"
 import "./Home.scss";
 
 class Home extends Component {
 
   state = {
     loggedIn: false,
-    joke: ""
   };
 
   componentDidMount() {
-    this.getJoke();
     this.loggedIn();
   }
 
-  getJoke = () => {
-    API.ChuckNorris().then(joke => {
-      let newJoke = joke.data.value.joke.replace(/&quot;/g, '"');
-      this.setState({
-        joke: newJoke
-      })
-    }).catch(err => {
-      console.log(err)
-    });
-  }
 
   loggedIn = () => {
     API.isLoggedIn().then(user => {
@@ -42,10 +28,6 @@ class Home extends Component {
   render() {
     return (
       <div className="homeBox">
-        <Joke joke={this.state.joke}/>
-        {this.state.loggedIn ? (
-          <Button onClick={e=> {this.getJoke()}} color="warning" block>Get New Joke</Button>
-        ) : (<></>)}
       </div>
     );
   }
