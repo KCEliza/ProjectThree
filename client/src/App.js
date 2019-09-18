6efdea88f74afcaba6285fe42e75a054c9f8714f
Christelle Mangama <mangama_christelle@yahoo.fr>
2019-09-17 19:57:16 -0600

    Adding new

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,28 @@ import Auth from "./pages/Auth";
 import NoMatch from "./pages/NoMatch";
 import TopNav from "./components/TopNav";
 import Footer from "./components/Footer";
-import SubmitIdea from "./components/SubmitIdea";
-// import { Container } from 'reactstrap';
+// import SubmitIdea from "./components/SubmitIdea";
+import { Container } from 'reactstrap';
 
 function App() {
   return (
-    <SubmitIdea />
-      // <Router>
-      //   <>
-      //     <TopNav />
-      //     {/* <Container> */}
-      //       <Switch>
-      //         <Route exact path="/" component={Home} />
-      //         <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
-      //         <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
-            
-      //         <Route exact path="/profile" component={Profile} />
-      //         <Route component={NoMatch} />t
-      //       </Switch>
-      //     {/* </Container> */}
-      //     <Footer />
-      //   </>
-      // </Router>
+   
+      <Router>
+        <>
+          <TopNav />
+          <Container>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
+              <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
+  
+              <Route exact path="/profile" component={Profile} />
+              <Route component={NoMatch} />t
+            </Switch>
+          </Container>
+          <Footer />
+        </>
+      </Router>
   )
 }
 
