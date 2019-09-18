fb1b3cfb0fee5e99cd517e06086dc2691473b518
Kona Marie <ambermoreyra@gmail.com>
2019-09-18 13:30:53 -0600

    created wrapper component

---
+++ client/src/components/Wrapper/Wrapper.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Wrapper(props) {
+ return (
+<div class="border">
+<div class="row">
+    <div class="col-md-8 offset-md-3">Put Sorting/Filtering Info Here</div>
+  </div>
+
+<div class="row">
+<div class="col-md-8 offset-md-3">Put Cards Here</div>
+</div>
+</div>
+ );
+}
+export default Wrapper;
