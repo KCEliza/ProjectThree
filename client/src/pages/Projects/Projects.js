be5fdd7e2b2d233b999cbf8d0d66dbe8f44540c3
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-09-26 13:42:00 -0600

    things are happening

--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,11 +1,66 @@
 import Menu from "../../components/Menu";
 
-class Projects extends Component{
+componentDidMount() {
+
+    this.loading();
+
+    API.isLoggedIn().then(user => {
+        if (user.data.loggedIn) {
+            this.setState({
+                loggedIn: true,
+                user: user.data.user
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+loading() {
+    setTimeout(() => {
+        this.setState({
+            loading: false
+        })
+    }, 1000)
+};
+
 render() {
-    return(
-        <div className = "title">
-        Coming Soon
-        </div>
+    return (
+
+        <>
+
+            {this.state.loggedIn ? (
+                <>
+                    <Menu />
+
+                    <div className="comingSoon">
+                
+                    Coming Soon
+                    </div>
+
+                </>
+            ) : (
+                <div className="noUser">
+                    {!this.state.loading ? (
+                        <>
+                            <div className="container text-center">
+
+                                <h1>please log in</h1>
+                                <Link className="loginLink" to="/login"><Button className="loginBtn btn-large" color="info" >Login</Button></Link>
+
+                            </div>
+                        </>
+                    ) : (
+                        <img id="loadingIcon" src="./assets/images/lightbulb.gif" alt="loading"/>
+                    )}
+                </div> 
+                
+            )}
+        </>
     )
 }
-}
+
+};
+
+
+export default Projects;
